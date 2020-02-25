ddf0b7ea7eebc6733fde316612f16bc35d863197
AlsumatMohammed <alsumatmohammed@gmail.com>
2020-02-25 06:11:59 +0300

    initial commit

--- app/src/main/java/com/example/final_project/splashcreen.java
+++ app/src/main/java/com/example/final_project/splashcreen.java
@@ -2,9 +2,12 @@ package com.example.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -41,8 +44,16 @@ public class splashcreen extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(splashcreen.this, login_screen.class);
-                startActivity(intent);
+
+                Pair[] pairs = new Pair[2];
+                pairs [0] = new Pair<View, String>(logo, "logo_image");
+                pairs [1] = new Pair<View, String>(tv1, "logo_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(splashcreen.this, pairs);
+
+                startActivity(intent, options.toBundle());
                 finish();
+
             }
         }, SPLASHTIME);
     }
